8adf1a2ac1f12f77c27ef28c0d947b1631f20bed
Jaiden Taylor <jtaylord17@gmail.com>
2024-07-03 06:08:36 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import dotenv
+import discord
+import os
+
+dotenv.load_dotenv()
+token = str(os.getenv("TOKEN"))
+
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready and online!")
+
+@bot.slash_command(name = "hello", description = "Say hello to the bot")
+async def hello(ctx:discord.ApplicationContext):
+    await ctx.respond("Hey!")
+
+bot.run(os.getenv('TOKEN'))
